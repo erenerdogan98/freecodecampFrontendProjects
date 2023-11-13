6b573c8b4573a72a3198376d248ca66f24b4cd7f
erenerdogan98 <eren.erdogannn@hotmail.com>
2023-11-13 23:34:57 +0300

    tek app.js ile yapınca hata düzeldi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import * as marked from 'marked';  // Import the marked library
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    // Default markdown content
+    const defaultMarkdown = `
+# Welcome to My Markdown Previewer!
+
+## Subheading
+
+[Link](https://www.example.com)
+
+\`inline code\`
+
+\`\`\`
+// code block
+const example = "Hello, World!";
+\`\`\`
+
+- List item 1
+- List item 2
+
+> Blockquote
+
+![Image](https://via.placeholder.com/150)
+
+**Bolded Text**
+    `;
+
+    setMarkdown(defaultMarkdown);
+  }, []);
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="editor-container">
+        <h2>Editor</h2>
+        <textarea id="editor" value={markdown} onChange={handleChange} />
+      </div>
+      <div className="preview-container">
+        <h2>Preview</h2>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked.marked(markdown) }}  // Use marked.marked
+        ></div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
